0291d859e2934e2044f7f8993e1f86c352bf5c12
mmerabet <mhmd.merabet@gmail.com>
2023-01-17 07:31:03 +0700

    fetching profile

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('app.js')
+// Initialize class
+const github = new GitHub();
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Add event listener
+searchUser.addEventListener('keyup', (event) => {
+  // Get the input
+  const userText = event.target.value;
+
+  if (userText !== '') {
+    github.getUser(userText)
+      .then(data => {
+        if (data.profile.message === 'Not Found') {
+          // Show alert
+
+        } else {
+          // Show profile
+        }
+      })
+  } else {
+    // Clear profile
+  }
+})
--- github.js
+++ github.js
@@ -1,1 +1,16 @@
-console.log('github.js')
+class GitHub {
+  constructor() {
+    this.client_id = '2b98ae8015ede7fd020c';
+    this.client_secret = '20504e967de1f56e097dacedeb544122e096c01c';
+  }
+
+  async getUser(user){
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile
+    }
+  }
+}
